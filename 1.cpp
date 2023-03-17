078d14c6d9e9206f58e94f5b16261bb5b40b9cbb
Lontion <llqljsdhd@foxmail.com>
2023-03-17 20:43:14 +0800

    new project version

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 17 Mar 2023 08:36:44 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, git\n";
+    return 0;
+}
